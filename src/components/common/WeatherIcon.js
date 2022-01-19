4da1a8644a7932b4aab0ae7b3ed7c6e29178f9c2
henriquebcustodio <henriquebcustodio@outlook.com>
2022-01-19 19:18:46 -0300

    Fixed weather icon flickering

--- src/components/common/WeatherIcon.js
+++ src/components/common/WeatherIcon.js
@@ -1,16 +1,41 @@
+import { Fragment } from 'react';
 import styled from 'styled-components';
 import getWeatherIcon from "../../assets/WeatherIcons/WeatherIcons";
+import ReactLoading from 'react-loading';
+import colorScheme from '../../colors/color-scheme';
 
 const Icon = styled.img`
     display: 'block';
     height: 100%;
 `;
 
+const LoadingWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 10rem;
+`;
+
 const WeatherIcon = props => {
     const iconSrc = getWeatherIcon(props.id, props.isNight);
 
     return (
-        <Icon src={iconSrc.default} alt="Weather Condition" />
+        <Fragment>
+            {props.isNight !== undefined ?
+                <Icon
+                    src={iconSrc.default}
+                    alt="Weather Condition"
+                /> : ''}
+            {props.isNight === undefined ?
+                <LoadingWrapper>
+                    <ReactLoading
+                        type={'spin'}
+                        color={colorScheme.blue}
+                        height={'2rem'}
+                        width={'2rem'} />
+                </LoadingWrapper> : ''}
+        </Fragment>
+
     );
 };
 
