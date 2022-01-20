ae97fe9a45491b27df174fe2d40565ffbee85f07
henriquebcustodio <henriquebcustodio@outlook.com>
2022-01-20 20:10:10 -0300

    Render different layouts based on screen width

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import Mobile from "./layouts/Mobile";
+import Desktop from "./layouts/Desktop";
 import { ThemeProvider } from "styled-components";
 import colorScheme from "./colors/color-scheme";
 import CityProvider from "./context/CityProvider";
 import WeatherProvider from "./context/WeatherProvider";
 import UnitsProvider from "./context/UnitsProvider";
+import useWindowWide from "./hooks/useWindowWide";
 
 const App = () => {
+    const isDesktop = useWindowWide(800);
+    console.log(isDesktop);
+
     return (
         <UnitsProvider>
             <CityProvider>
                 <WeatherProvider>
                     <ThemeProvider theme={colorScheme}>
-                        <Mobile />
+                        {!isDesktop &&
+                            <Mobile />
+                        }
+                        {isDesktop &&
+                            <Desktop />
+                        }
                     </ThemeProvider>
                 </WeatherProvider>
             </CityProvider>
